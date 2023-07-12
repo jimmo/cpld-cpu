39f9548097bdfee7045f9c7f67b9b57c6b450525
Jim Mussared <jim.mussared@gmail.com>
2023-07-12 13:06:08 +1000

    programmer/main.py: Add pin descriptions.

--- programmer/main.py
+++ programmer/main.py
@@ -3,10 +3,23 @@ import time
 
 speed = 20
 
+# 7-pin programming header is:
+# 0  (unused)       Yellow
+# 1  clock    Y9    Green
+# 2  data     Y10   Blue
+# 3  latch    Y11   Purple
+# 4  reset    Y12   Grey
+# 5  (unused)       White
+# GND               Black
+
+ # prog 4
 rst = machine.Pin('Y12', mode=machine.Pin.OUT, pull=machine.Pin.PULL_NONE, value=1)
 
+# prog 1
 clk = machine.Pin('Y9', mode=machine.Pin.OUT, pull=machine.Pin.PULL_NONE)
+# prog 2
 data = machine.Pin('Y10', mode=machine.Pin.OUT, pull=machine.Pin.PULL_NONE)
+# prog 3
 latch = machine.Pin('Y11', mode=machine.Pin.OUT, pull=machine.Pin.PULL_NONE)
 
 def reset():
@@ -19,10 +32,10 @@ def pgm(on=True):
   else:
     rst.value(1)
 
-def send(x):  
+def send(x):
   clk.value(0)
   latch.value(0)
-  
+
   for i in range(8):
     bit = (x >> (7-i)) & 1
     data.value(bit)
@@ -39,7 +52,7 @@ def send(x):
   time.sleep_us(speed)
   latch.value(0)
 
-  
+
 def erase():
   reset()
   pgm(True)
@@ -49,7 +62,7 @@ def erase():
   reset()
   reset()
 
-  
+
 def load(data):
   reset()
   pgm(True)
